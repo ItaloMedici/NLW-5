b440a1f7d521fa6e68630967d9206a835a52d4a9
ItaloMedici <italomedice969@gmail.com>
2021-04-25 15:08:14 -0300

    create new functions on app

--- NLW#5/podcastr/src/pages/_app.tsx
+++ NLW#5/podcastr/src/pages/_app.tsx
@@ -4,17 +4,50 @@ import '../styles/global.scss';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 
+import { PlayerContext } from '../contexts/playerContext';
+
 import styles from '../styles/app.module.scss'
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEspisodeIndex, setcurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+
+  function play (episode){
+    setEpisodeList([episode]);
+    setcurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setIsPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider 
+      value={{ 
+        episodeList, 
+        currentEspisodeIndex, 
+        play, 
+        isPlaying, 
+        togglePlay,
+        setIsPlayingState
+      }}>
+
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 }
 
